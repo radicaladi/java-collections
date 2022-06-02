3c4b369edea1ee7b8be3beab6ef97ebf606d9410
Adrian Brown <adrian.r.brown90@gmail.com>
2022-06-01 18:18:34 -0700

    adding first java collection program exercise

---
+++ src/src/colors.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+// Write a Java program to create a new array list,
+// add some colors (string) and print out the collection
+
+public class colors {
+    public static void main(String[] args) {
+        List<String> colors = new ArrayList<>();
+        colors.add("red");
+        colors.add("green");
+        colors.add("blue");
+        colors.add("yellow");
+        System.out.println(colors);
+    }
+}
