f2ae11ee9480aaaecfd69b7c8e74851d860f619b
Adrian Brown <adrian.r.brown90@gmail.com>
2022-06-22 21:38:56 -0700

    adding program to aempty an array list

--- src/src/cars.java
+++ src/src/cars.java
@@ -14,6 +14,14 @@ public class cars {
 
         LinkedList<String> joinedList = linking_two_lists(carTypes, carTypes2);
         System.out.println("New linked list: " + joinedList);
+
+        ArrayList<String> autos = new ArrayList<>();
+        autos.add("Audi");
+        autos.add("BMW");
+        autos.add("Volkswagen");
+        System.out.println("Autos before emptied: " + autos);
+        emptyList(autos);
+        System.out.println("Autos emptied: " + autos);
     }
 
     public static LinkedList<String> linking_two_lists(LinkedList<String> listOne, LinkedList<String> listTwo) {
@@ -22,5 +30,10 @@ public class cars {
         joinedList.add(String.valueOf(listTwo));
         return joinedList;
         }
+
+    //Write a Java program to empty an array list.
+    public static void emptyList (ArrayList<String> list) {
+        list.removeAll(list);
+    }
 
 }
