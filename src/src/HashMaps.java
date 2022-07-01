43ddff9e6aea8524ac73e0f4f4b1a731387d2e45
Adrian Brown <adrian.r.brown90@gmail.com>
2022-06-30 20:57:00 -0700

    adding program to associate key value pairs of a HashMap

--- src/src/HashMaps.java
+++ src/src/HashMaps.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class HashMaps {
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMaps {
+    public static void main(String[] args) {
+        HashMap<Integer, String> jordanShoes = new HashMap<>();
+        jordanShoes.put(1, "Carmine");
+        jordanShoes.put(2, "Raptor");
+        jordanShoes.put(3, "Concord");
+
+        hashMapKeyValuePairs(jordanShoes);
+
+    }
+
+    // Write a Java program to associate the specified value with the specified key in a HashMap.
+    public static void hashMapKeyValuePairs (HashMap<Integer, String> map) {
+        for(Map.Entry x:map.entrySet()){
+            System.out.println(x.getKey()+" "+x.getValue());
+        }
+    }
 }
