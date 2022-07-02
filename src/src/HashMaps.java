9303f15b184590684085c74308fdc2efe5bc4054
Adrian Brown <adrian.r.brown90@gmail.com>
2022-07-01 21:06:29 -0700

    adding program to iterate through all elements of a hashmap

--- src/src/HashMaps.java
+++ src/src/HashMaps.java
@@ -1,5 +1,4 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class HashMaps {
     public static void main(String[] args) {
@@ -10,12 +9,23 @@ public class HashMaps {
 
         hashMapKeyValuePairs(jordanShoes);
 
+        Iterator<Map.Entry<Integer, String>> jordanIterator = jordanShoes.entrySet().iterator();
+        iterateAllElementsInHashMap(jordanIterator);
+
     }
 
     // Write a Java program to associate the specified value with the specified key in a HashMap.
     public static void hashMapKeyValuePairs (HashMap<Integer, String> map) {
         for(Map.Entry x:map.entrySet()){
             System.out.println(x.getKey()+" "+x.getValue());
+        }
+    }
+
+    //Write a Java program to iterate through all elements in a hash list.
+    public static void iterateAllElementsInHashMap (Iterator<Map.Entry<Integer, String>> iterator) {
+        while(iterator.hasNext()) {
+            System.out.print(iterator.next());
+            System.out.print(", ");
         }
     }
 }
